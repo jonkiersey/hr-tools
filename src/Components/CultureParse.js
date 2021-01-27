2da397057dbb207caea84ea8f8e80db3d36e01cc
Jon Kiersey <jon.kiersey@gmail.com>
2021-01-26 18:19:34 -0800

    Fix 1 word entry split bug; fix starting numbers not adding for first line; add multiples in subsequent entries; add clear button

--- src/Components/CultureParse.js
+++ src/Components/CultureParse.js
@@ -2,94 +2,109 @@ import React, { useEffect, useState } from 'react';
 import AutosizeInput from 'react-input-autosize';
 
 const CultureParse = () => {
-  const [formData, setFormData] = useState('');
+  const [sentences, setSentences] = useState([]);
   const [rawData, setRawData] = useState('');
   const [keyData, setKeyData] = useState({});
-  const [currentText, setCurrentText] = useState('');
   const [textToSave, setTextToSave] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [previousIndex, setPreviousIndex] = useState(0);
-  const [startingNumber, setStartingNumber] = useState(1);
+  const [occurencesFromString, setOccurencesFromString] = useState(1);
 
   useEffect(() => {
-    setRawData(formData);
-  }, [formData])
-
-  useEffect(() => {
-    if (!rawData) { return; }
-    const newIndex = rawData.indexOf('.') + 1;
-    const firstSentence = rawData.slice(0, newIndex - 1);
-    setCurrentIndex(newIndex);
-    setCurrentText(firstSentence);
-    setTextToSave(firstSentence);
+    setSentences(rawData.split('.').map((sentence) => sentence.trim()));
+    setCurrentIndex(0);
   }, [rawData]);
+
+  const clear = () => {
+    setRawData('');
+    setSentences([]);
+    setKeyData({});
+    setTextToSave('');
+    setCurrentIndex(0)
+    setOccurencesFromString(1);
+  };
+
+  useEffect(() => {
+    if (!sentences.length) { return; }
+    const nextSentence = sentences[0];
+    const [startParen, endParen] = [nextSentence.indexOf('('), nextSentence.indexOf(')')];
+    let maybeNumber;
+    if (startParen >= 0 && endParen > 0) {
+      const parenContent = nextSentence.substring(startParen + 1, endParen);
+      console.log(parenContent);
+      maybeNumber = parseInt(parenContent, 10);
+    }
+    setOccurencesFromString(maybeNumber ? maybeNumber : 1);
+    setTextToSave(nextSentence);
+  }, [sentences]);
 
   const prettyPrint = () => {
     const sortedData = Object.entries(keyData).sort((a, b) => b[1] - a[1]);
-    const prettyData = sortedData.map(([key, count]) => (
+    const prettyData = sortedData.map(([key, count]) => {
+      const firstWord = key.includes(' ') ? key.slice(0, key.indexOf(' ')) : key;
+      const rest = key.includes(' ') && ` ${key.slice(key.indexOf(' ')).trim()}`;
+      return (
       <>
         <span style={{fontWeight: 'bold'}}>
-          {count > 1 && ` (${count})`} {key.slice(0, key.indexOf(' ')).trim()}
+          {count > 1 && ` (${count})`} {firstWord}
         </span>
-        &nbsp;{key.slice(key.indexOf(' ')).trim()}.
+        {rest}.
       </>
-      ));
+      );
+    });
     return prettyData;
   };
 
   const advanceCurrent = () => {
-    const newIndex = rawData.slice(currentIndex).indexOf('.') + currentIndex + 1;
-    const nextSentence = rawData.slice(currentIndex, newIndex - 1);
+    const newIndex = currentIndex + 1;
+    const nextSentence = sentences[newIndex];
     const [startParen, endParen] = [nextSentence.indexOf('('), nextSentence.indexOf(')')];
     let maybeNumber;
-    if (startParen > 0 && endParen > 0) {
+    if (startParen >= 0 && endParen > 0) {
       const parenContent = nextSentence.substring(startParen + 1, endParen);
+      console.log(parenContent);
       maybeNumber = parseInt(parenContent, 10);
-      if (maybeNumber) {
-        setStartingNumber(maybeNumber);
-      }
     }
-    setStartingNumber(maybeNumber ? maybeNumber : 1);
-    setPreviousIndex(currentIndex);
+    setOccurencesFromString(maybeNumber ? maybeNumber : 1);
     setCurrentIndex(newIndex);
-    setCurrentText(nextSentence);
     setTextToSave(nextSentence);
   };
 
   const combine = () => {
-    const newIndex = rawData.slice(currentIndex).indexOf('.') + currentIndex + 1;
-    const nextSentence = rawData.slice(previousIndex, newIndex - 1);
+    const newIndex = currentIndex + 1;
+    const nextSentence = `${textToSave}. ${sentences[newIndex]}`;
     setCurrentIndex(newIndex);
-    setCurrentText(nextSentence);
     setTextToSave(nextSentence);
   };
 
   const newEntry = () => {
-    setKeyData({ ...keyData, [textToSave.trim()]: startingNumber });
+    setKeyData({ ...keyData, [textToSave.trim()]: occurencesFromString });
     advanceCurrent();
   };
 
   const incrementKey = (key) => {
     const oldCount = keyData[key];
-    setKeyData({ ...keyData, [key]: oldCount + 1 });
+    setKeyData({ ...keyData, [key]: oldCount + occurencesFromString });
     advanceCurrent();
   };
 
   return (
     <div className="App">
-      <div style={{background: '#AF9CE5', color: 'white'}}>
+      <div style={{ background: '#AF9CE5', color: 'white' }}>
         <h1>Culture Parse</h1>
+      </div>
+      <div style={{ textAlign: 'right' }}>
+        <button onClick={clear}>Clear</button>&nbsp;
       </div>
       Enter text to be parsed below:
       <div>
-        <textarea type="text" value={rawData} onChange={(event) => setFormData(event.target.value)} rows="4" cols="50" />
+        <textarea type="text" value={rawData} onChange={(event) => setRawData(event.target.value)} rows="3" cols="50" />
       </div>
       &nbsp;
       <div>
         <AutosizeInput type="text" value={textToSave} onChange={(event) => setTextToSave(event.target.value)} />&nbsp;
       </div>
       <div>
-        <input type="number" style={{ width: 50 }} value={startingNumber} onChange={(event) => setStartingNumber(parseInt(event.target.value, 10))} />
+        <input type="number" style={{ width: 49 }} value={occurencesFromString} onChange={(event) => setOccurencesFromString(parseInt(event.target.value, 10))} />
       </div>
       <div>
         <button onClick={advanceCurrent}>Next/Ignore</button>&nbsp;
